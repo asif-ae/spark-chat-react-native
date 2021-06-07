7d7a0055298b4805547d0162cf8867c20c94ffa3
Asif Estiak <me.asif.ae@gmail.com>
2021-06-07 18:13:25 +0600

    initialized vector icon

--- App.js
+++ App.js
@@ -3,14 +3,38 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+// Global Screen Options
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: '#2C6BED' },
+  headerTitleStyle: { color: 'white' },
+  headerTintColor: 'white',
+}
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen
+          options={{
+            title: `Let's Sign Up`,
+          }}
+          name='Login'
+          component={LoginScreen}
+        ></Stack.Screen>
+      </Stack.Navigator>
+      {/* <Stack.Screen name='Login'>
+        <LoginScreen></LoginScreen>
+      </Stack.Screen> */}
+      {/* <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
-      </View>
+      </View> */}
     </NavigationContainer>
   );
 }
