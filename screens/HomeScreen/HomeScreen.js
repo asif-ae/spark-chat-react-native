d4cca94af20f74459a57cbf9d7855148ff207b5a
Asif Estiak <me.asif.ae@gmail.com>
2021-06-08 12:09:43 +0600

    added home screen

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, SafeAreaView } from 'react-native'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>This is the Home Page</Text>
-    </View>
+    <SafeAreaView>
+      {/* <StatusBar style='light' /> */}
+      <ScrollView>
+        <Text>Home</Text>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
